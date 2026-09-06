53351ebf0ab2e3639bc7751a3e32d8f3b1397c1e
mclark <maryariley4578@yahoo.com>
2026-09-06 10:48:31 -0400

    Completed the inheritance challenge

--- asgn02-inheritance/challenge-2-inheritance.php
+++ asgn02-inheritance/challenge-2-inheritance.php
@@ -1,1 +1,51 @@
-test
+<?php
+
+//parent class
+class Animal
+{
+  public $name;
+  public $species;
+
+  //give animal name and species
+  public function describeAnimal($name, $species)
+  {
+    $this->name = $name;
+    $this->species = $species;
+  }
+}
+
+class Dog extends Animal
+{
+  //give dog breed
+  public $breed;
+
+  public function bark()
+  {
+    return "Woof! My name is " . $this->name . ", the " . $this->breed . " " . $this->species . ".";
+  }
+}
+
+class Cat extends Animal
+{
+  //give cat color
+  public $color;
+
+  public function meow()
+  {
+    return "Meow! My name is " . $this->name . ", the " . $this->color . " " . $this->species . ".";
+  }
+}
+
+$dog1 = new Dog();
+$dog1->describeAnimal("Buddy", "Canine");
+$dog1->breed = "Golden Retriever";
+
+$cat1 = new Cat();
+$cat1->describeAnimal("Whiskers", "Feline");
+$cat1->color = "Orange";
+
+echo $dog1->bark() . "<br>";  // Output: Woof! My name is Buddy, the Golden Retriever Canine.
+echo $cat1->meow() . "<br>"; // Output: Meow! My name is Whiskers, the Orange Feline.
+
+echo get_parent_class($dog1) . "<br>"; // Output: Animal
+echo get_parent_class($cat1) . "<br>"; // Output: Animal
