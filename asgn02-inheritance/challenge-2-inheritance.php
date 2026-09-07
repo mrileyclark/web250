b91e1c4cc87c263678a26c55787ea6a48da64108
mclark <maryariley4578@yahoo.com>
2026-09-07 12:55:09 -0400

    Completed the access challenge part 2

--- asgn02-inheritance/challenge-2-inheritance.php
+++ asgn02-inheritance/challenge-2-inheritance.php
@@ -3,17 +3,52 @@
 //parent class
 class Animal
 {
-  public $name;
-  public $species;
+  //public $name;
+  //public $species;
+  //public $age;
 
-  //give animal name and species
+  private $name;
+  protected $species;
+  private $age;
+
+
+  //give animal name and species 
   public function describeAnimal($name, $species)
   {
     $this->name = $name;
     $this->species = $species;
   }
+
+  // getters for private properties
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  //setter for private age
+  public function setAge($age)
+  {
+    //only set age if it's a non-negative value
+    if ($age >= 0) {
+      $this->age = $age;
+    }
+  }
+
+  //getter for private age
+  public function getAge()
+  {
+    return $this->age;
+  }
+
+  //all animals can sleep
+  public function sleep()
+  {
+    return $this->name . " is sleeping.";
+  }
 }
 
+
+//subclass 
 class Dog extends Animal
 {
   //give dog breed
@@ -21,7 +56,14 @@ class Dog extends Animal
 
   public function bark()
   {
-    return "Woof! My name is " . $this->name . ", the " . $this->breed . " " . $this->species . ".";
+    //use getters to access private properties name and age
+    return "Woof! My name is " . $this->getName() . ", the " . $this->breed . " " . $this->species . ". I am " . $this->getAge() . " years old.";
+  }
+
+  public function fetch()
+  {
+    //use getter to access private name property
+    return $this->getName() . " is fetching the ball!";
   }
 }
 
@@ -32,20 +74,34 @@ class Cat extends Animal
 
   public function meow()
   {
-    return "Meow! My name is " . $this->name . ", the " . $this->color . " " . $this->species . ".";
+    //use getters to access private properties name and age
+    return "Meow! My name is " . $this->getName() . ", the " . $this->color . " " . $this->species . ". I am " . $this->getAge() . " years old.";
+  }
+
+  public function climb()
+  {
+    //use getter to access private name property
+    return $this->getName() . " is climbing the tree!";
   }
 }
 
 $dog1 = new Dog();
 $dog1->describeAnimal("Buddy", "Canine");
+$dog1->setAge(5);
 $dog1->breed = "Golden Retriever";
 
 $cat1 = new Cat();
 $cat1->describeAnimal("Whiskers", "Feline");
+$cat1->setAge(3);
 $cat1->color = "Orange";
 
 echo $dog1->bark() . "<br>";  // Output: Woof! My name is Buddy, the Golden Retriever Canine.
-echo $cat1->meow() . "<br>"; // Output: Meow! My name is Whiskers, the Orange Feline.
+echo $dog1->fetch() . "<br>"; // Output: Buddy is fetching the ball!
+echo $dog1->sleep() . "<br>"; // Output: Buddy is sleeping.
 
-echo get_parent_class($dog1) . "<br>"; // Output: Animal
-echo get_parent_class($cat1) . "<br>"; // Output: Animal
+echo $cat1->meow() . "<br>"; // Output: Meow! My name is Whiskers, the Orange Feline.
+echo $cat1->climb() . "<br>"; // Output: Whiskers is climbing the tree!
+echo $cat1->sleep() . "<br>"; // Output: Whiskers is sleeping.
+
+echo get_parent_class($dog1) . "<br>";
+echo get_parent_class($cat1) . "<br>";
